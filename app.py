46ee5eae43250584994d567d4be3fa63c3382a1a
3MK <55490349+3minka@users.noreply.github.com>
2023-10-26 18:29:32 +0900

    second changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import jsonify, request ## api 만들기 위한 라이브러리 
+
 
 app = Flask(__name__)
 
@@ -16,5 +18,39 @@ def hello_user(username):
 def show_feed(feed_id):
     return f"<h1> FeedID: {feed_id} </h1>"
 
+
+############################################### api db 만들기 수업 10/26
+# CRUD : CREAT(POST), READ(GET), UPDATA(UPDATAE), DELETE(DEL)
+# GET : 데이터를 요청할 때,
+# POST : CREATE, 데이터 생성할 때,
+@app.route("/api/v1/feeds", methods=['GET']) # 요청을 줘 api 입니다.
+
+def get_all_feeds():  # 전체 데이터를 주자
+
+    # DB에서 불러주는 것으로 
+    data = {
+        'status' : 'success',
+        'feed' : {
+            "feed1" : "data",
+            "feed2" : "data"
+        }
+    }
+    return jsonify(data)
+
+# JSON -> 프로토콜, 컴퓨터와 컴퓨터간의 상호작용을 위해 만들어짐. 
+# 핸드폰 -> 조명 껐다 켰다 할 수 있는 거의 소통 방식 
+
+## POST
+@app.route('api/v1/feeds', method=['POST'])
+def create_feed():
+    # request : 유저가 보낸 데이터
+    subject = request.form['subject']
+    content = request.form['content']
+
+    return jsonify({"result" : "success"})
+###############################################
+
 if __name__ == "__main__":
     app.run()
+
+#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
---
+++ app2.py
@@ -0,0 +1,52 @@
+# 서비스를 빨리 구현해야 하는 상황 -> flask
+
+from flask import Flask, request, render_template
+import sqlite3
+
+# flask 가동
+app = Flask(__name__)
+
+# sqlite 연결 mysql, mongodb
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+# db 만들기, 데이터베이스 테이블 생성
+def create_table():
+    print("DB 생성중 ...")
+    conn = get_db_connection()
+    cursor = conn.cursor() # 커서, 
+
+    # 쿼리문 작성
+    sql = f"CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT)"
+    cursor.execute(sql) # 쿼리문 실행
+    conn.commit() # 쿼리 반영하기
+    conn.close() # 닫아주기 
+
+
+# add_book
+def add_book(title, author):
+    conn = get_db_connection()
+    cursor = conn.cursor() # 커서, 
+    sql = f'INSERT INTO books (title, author) VALUES ('{title}', '{author}')'
+    cursor.execute(sql) # 쿼리문 실행
+    conn.commit()
+    conn.close()
+    
+
+@app.route('/', methods = ['GET', 'POST'])
+def main():
+    create_table()
+    if request.method == 'POST':
+        print("유저로부터 POST 요청 받는 중 ...")
+        title = request.form['title']
+        author = request.form['quthor']
+        add_book(title, author)
+
+    return render_template('index.html') # render 그려주기 
+
+if __name__ == "__main__":
+    app.run()
+
